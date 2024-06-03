f652cc5b6d3005d3cc86aef62229b01255fb8dc6
PASTELBOX <147224562+PASTELBOX@users.noreply.github.com>
2024-06-03 13:59:54 +0900

    tiles 작성

--- src/main/java/com/example/demo/TeamProjectApplication.java
+++ src/main/java/com/example/demo/TeamProjectApplication.java
@@ -1,15 +1,21 @@
 package com.example.demo;
 
+
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+
 @SpringBootApplication
-@ComponentScan({"com.example.demo","com.data.*","naver.cloud"})
+@ComponentScan({"com.example.demo","data.*","naver.cloud","tiles.config"})
+@MapperScan("data.mapper")
 public class TeamProjectApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(TeamProjectApplication.class, args);
 	}
+
 
 }
