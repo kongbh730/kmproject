28c136e81529f3abbcaeed6324ad68fcf3adfeb9
kongbh730 <kongbh730@gmail.com>
2024-06-03 14:13:54 +0900

    commit changes

--- src/main/java/com/example/demo/TeamProjectApplication.java
+++ src/main/java/com/example/demo/TeamProjectApplication.java
@@ -1,11 +1,13 @@
 package com.example.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan({"com.example.demo","com.data.*","naver.cloud"})
+@MapperScan("data.mapper")
 public class TeamProjectApplication {
 
 	public static void main(String[] args) {
