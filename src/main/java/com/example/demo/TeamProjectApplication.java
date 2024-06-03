cfcfeb840834344620129c0748f2047d5518c9ae
PASTELBOX <147224562+PASTELBOX@users.noreply.github.com>
2024-06-03 14:17:27 +0900

    Update TeamProjectApplication.java
    오류 수정

--- src/main/java/com/example/demo/TeamProjectApplication.java
+++ src/main/java/com/example/demo/TeamProjectApplication.java
@@ -1,9 +1,5 @@
 package com.example.demo;
 
-<<<<<<< HEAD
-=======
-
->>>>>>> cbbfef5d5b8029c5bc055ad1178eb15bcf2d1ae7
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,11 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-<<<<<<< HEAD
-@ComponentScan({"com.example.demo","com.data.*","naver.cloud"})
-=======
 @ComponentScan({"com.example.demo","data.*","naver.cloud","tiles.config"})
->>>>>>> cbbfef5d5b8029c5bc055ad1178eb15bcf2d1ae7
 @MapperScan("data.mapper")
 public class TeamProjectApplication {
 
